66988afa9e5064b89b452949acb37f363c0ddaa0
beldon <1047325205@qq.com>
2023-02-03 21:34:38 +0800

    play

--- app/src/main/java/me/beldon/learninfo/FirstFragment.java
+++ app/src/main/java/me/beldon/learninfo/FirstFragment.java
@@ -1,5 +1,9 @@
 package me.beldon.learninfo;
 
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,6 +31,8 @@ public class FirstFragment extends Fragment {
 
     }
 
+    private boolean light = false;
+
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
@@ -37,7 +43,37 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mediaPlayer = MediaPlayer.create(FirstFragment.this.getContext(), R.raw.abb);
+                mediaPlayer.start();
+            }
+        });
+
+        CameraManager cameraManager = (CameraManager) this.getContext().getSystemService(Context.CAMERA_SERVICE);
+
+        binding.light.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    if (!light) {
+                        cameraManager.setTorchMode("0", true);
+                        binding.light.setText("关灯");
+                        light = true;
+                    } else {
+                        cameraManager.setTorchMode("0", false);
+                        binding.light.setText("开灯");
+                        light = false;
+                    }
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
 
     @Override
     public void onDestroyView() {
